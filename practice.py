871b692ebe8c231a18c248d97885765d2aac8168
Wafa Sehar <papijabibts@gmail.com>
2026-09-05 13:31:20 +0530

    Add beginner Python projects

---
+++ practice.py
@@ -0,0 +1,51 @@
+#facewash = input("what is your fav facewash? ")
+#moisturizer = input("what is your fav moisturizer? ")
+#concealer = input("what is your fav concealer? ")
+
+#defining a function:
+#def cube(num):
+#    return num*num*num
+#print(cube(5))
+
+#tuples:
+#relatives = ("mom", "dad", "brother", "sister")
+#print(relatives[2])
+
+#comparing if statements:
+#def max_working_employee(hour1, hour2, hour3):
+   # if hour1 >= hour2 and hour1 >= hour3:
+    #    return "Sam has worked for the most hours."
+    #elif hour2 >= hour1 and hour2 >= hour3:
+     #   return "Joe is the hardworking employee."
+    #else:
+     #   return "Lucy is the decent worker."
+#print(max_working_employee(4000, 3000, 2000, 7000))
+
+#Calculator using if statements
+#number1 = (float(input("Enter Your First Operand: ")))
+#operator = (input("Enter The Operation: "))
+#number2 = (float(input("Enter Your Second Operand: ")))
+#if operator == "+":
+#    print(number1 + number2)
+#elif operator == "-":#
+    #print(number1 - number2)
+#elif operator == "/":
+#           print(number1 / number2)
+#elif operator =="*":
+#           print(number1 * number2)
+#else:
+#    print("Invalid Operator")
+
+#Create Dictionaries
+  #HTTP status codes
+#status_codes = {
+# "200": "ok",
+#  "300": "its a direction",
+#   "400": "wrong request",
+#    "500": "server error"
+#}
+  #student ids
+#student_profile = {"name": "Waffles", "id": "0001", "age": "17","phones": {"home": '1234567890',"office": '0987654321'}}
+#print(status_codes.keys())
+#print(student_profile.pop("age"))
+
